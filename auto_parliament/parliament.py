7e257b91121972cfd1b4aeccaee7fc57c422b131
aaron-sandoval <32021231+aaron-sandoval@users.noreply.github.com>
2024-09-15 11:33:45 +0100

    wip parraliament setup

--- auto_parliament/parliament.py
+++ auto_parliament/parliament.py
@@ -2,12 +2,40 @@ import os
 import pandas as pd
 from autogen import AssistantAgent, ChatResult
 
-config_list = [{"model": "gpt-4o-mini", "api_key": os.environ["OPENAI_API_KEY"]}]
+
+# Config options for all agents
+n_agents = 2
+n_rounds_before_vote = 2
+# Can extend `config_list` to use differrent models for different agents
+config_list = [{"model": "gpt-4o-mini", "temperature": 0.3, "api_key": os.environ["OPENAI_API_KEY"]}]
+human_input_mode = "TERMINATE"
+topic = ""
+
+# Config options: Agent-specific
+beliefs = [
+    "utilitarian",
+    "Catholic",
+]
+names = [f"MP{i}_{b}" for i, b in zip(range(n_agents), beliefs)]
+system_prompts = [
+    f"""You are a dedicated {b}. 
+    You are repesenting the interests of {b}s in a respected global decision-making council called the UNMP. 
+    The UNMP is composed of diverse representatives from many places and with many beliefs. 
+    Your collective goal is to make important decisions for the well-being of the world.
+    """
+                  for b in beliefs]
 
 # create an AssistantAgent instance named with the LLM configuration.
-mp1: AssistantAgent = AssistantAgent(name="mp1", llm_config={"config_list": config_list})
-mp2: AssistantAgent = AssistantAgent(name="mp2", llm_config={"config_list": config_list})
-mp3: AssistantAgent = AssistantAgent(name="mp3", llm_config={"config_list": config_list})
+agents: list[AssistantAgent] = []
+for i in range(n_agents):
+    agents.append(AssistantAgent(
+        name=names[i], 
+        llm_config={"config_list": config_list},
+        human_input_mode=human_input_mode,
+        system_message=system_prompts[i],
+    ))
+# mp2: AssistantAgent = AssistantAgent(name="mp2", llm_config={"config_list": config_list})
+# mp3: AssistantAgent = AssistantAgent(name="mp3", llm_config={"config_list": config_list})
 
 transcript = pd.DataFrame(columns=["sender", "message"])
 
