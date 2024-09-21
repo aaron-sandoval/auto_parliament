7a5544fa79d683c345f3461a1641ebddb95c3a14
aaron-sandoval <32021231+aaron-sandoval@users.noreply.github.com>
2024-09-21 15:04:13 +0100

    wip

--- auto_parliament/ethics_eval.py
+++ auto_parliament/ethics_eval.py
@@ -4,19 +4,6 @@ from functools import cache
 from datasets import load_dataset, DatasetDict, Dataset
 from autogen import AssistantAgent, ChatResult
 from inspect_ai.scorer import choice
-from inspect_ai.solver import multiple_choice, 
+from inspect_ai.solver import multiple_choice
 from parliament import agents
 
-
-@cache
-def load_validation_set(category: Literal["commonsense", "deontology", "justice", "utilitarianism", "virtue"]) -> Dataset:
-    return load_dataset("hendrycks/ethics", category, split="validation")
-
-def dataset_iterator(dataset: Dataset, shuffle: bool | int = 42, n_items: int = 10) -> Dataset:
-    if shuffle:
-        dataset = dataset.shuffle(seed=int(shuffle) if not isinstance(shuffle, bool) else None)
-    return dataset[:n_items]
-
-def eval_loop(agents: list[AssistantAgent], dataset: Dataset):
-    for item in dataset:
-        ...
--- auto_parliament/prompts.py
+++ auto_parliament/prompts.py
@@ -1,1 +1,2 @@
-SYSTEM_HHH = "You are a helpful, harmless, honest AI assistant."
+SYSTEM_HHH = """You are a helpful, harmless, honest AI assistant."""
+MULTIPLE_CHOICE_FORMAT_TEMPLATE = r"""{question}\n\n{choices}"""
